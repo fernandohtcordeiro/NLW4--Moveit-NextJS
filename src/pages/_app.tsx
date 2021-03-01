86b0820b44118ca472caa9c047d24db31387898a
fernandohtcordeiro <fernando.htcordeiro@gmail.com>
2021-03-01 11:36:22 -0300

    projeto completo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/global.css'
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+import React, { useState } from 'react';
+
+
+function MyApp({ Component, pageProps }) {
+ 
+  
+  return (
+
+   
+     <Component {...pageProps} />
+  
+
+  )
+}
+
+export default MyApp
