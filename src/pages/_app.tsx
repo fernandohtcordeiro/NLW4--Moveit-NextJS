d9aa4661eadb595957d2a356152d937e5b02606e
fernandohtcordeiro <fernando.htcordeiro@gmail.com>
2021-03-02 20:33:14 -0300

    Atualização app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/global.css';
+import '../styles/global.css'
+
+import { ChallengesContext, ChallengeProvider } from '../contexts/ChallengesContext'
+import React, { useState } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
+ 
+  
   return (
-    <Component {...pageProps} />
+
+   
+     <Component {...pageProps} />
+  
+
   )
 }
 
